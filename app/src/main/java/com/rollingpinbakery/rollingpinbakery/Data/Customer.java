7d535e51f6b158a9fadea8c98119f7431c86705d
rudsteven <rudsteven@gmail.com>
2018-02-12 15:57:28 -0600

    Customer Class for Database

---
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/Customer.java
@@ -0,0 +1,61 @@
+package com.rollingpinbakery.rollingpinbakery.Data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+/**
+ * Created by rudst on 2/12/2018.
+ */
+
+@Entity
+public class Customer {
+
+    @PrimaryKey(autoGenerate = true)
+    private int _custId;
+
+    @ColumnInfo(name = "custFname")
+    private String custFName;
+
+    @ColumnInfo(name = "custLName")
+    private String custLName;
+
+    @ColumnInfo(name = "custUsername")
+    private String custUsername;
+
+    @ColumnInfo(name = "custPassword")
+    private String custPassword;
+
+    @ColumnInfo(name = "dateSignedUp")
+    private Date dateSignedUp;
+
+
+    public Customer(String custFName, String custLName, String custUsername, String custPassword, Date dateSignedUp) {
+        this.custFName = custFName;
+        this.custLName = custLName;
+        this.custUsername = custUsername;
+        this.custPassword = custPassword;
+        this.dateSignedUp = dateSignedUp;
+    }
+
+    public int get_custId() {return _custId;}
+    public void set_custId(int _custId) {this._custId = _custId;}
+
+    public String getCustFName() {return custFName;}
+    public void setCustFName(String custFName) {this.custFName = custFName;}
+
+    public String getCustLName() {return custLName;}
+    public void setCustLName(String custLName) {this.custLName = custLName;}
+
+    public String getCustUsername() {return custUsername;}
+    public void setCustUsername(String custUsername) {this.custUsername = custUsername;}
+
+    public String getCustPassword() {return custPassword;}
+    public void setCustPassword(String custPassword) {this.custPassword = custPassword;}
+
+    public Date getDateSignedUp() {return dateSignedUp;}
+    public void setDateSignedUp(Date dateSignedUp) {this.dateSignedUp = dateSignedUp;}
+
+}
