3886de916d0ef646c0b9e34cba81e075eacc75bb
rudsteven <rudsteven@gmail.com>
2018-02-12 17:31:14 -0600

    Added Product Class and ProductDao for RoomDatabase.

---
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/Product.java
@@ -0,0 +1,71 @@
+package com.rollingpinbakery.rollingpinbakery.Data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by rudst on 2/12/2018.
+ */
+
+@Entity
+public class Product {
+    @PrimaryKey(autoGenerate = true)
+    private int _prodId;
+
+    @ColumnInfo(name = "prodName")
+    private String prodName;
+
+    @ColumnInfo(name = "prodRetailPrice")
+    private Double prodRetailPrice;
+
+    @ColumnInfo(name = "prodSalePrice")
+    private Double prodSalePrice;
+
+    @ColumnInfo(name = "prodDesc")
+    private String prodDesc;
+
+    @ColumnInfo(name = "prodType")
+    private String prodType;
+
+    @ColumnInfo(name = "prodFeatured")
+    private Boolean prodFeatured;
+
+    @ColumnInfo(name = "prodImg")
+    private String prodImg;
+
+    public Product(int _prodId, String prodName, Double prodRetailPrice, Double prodSalePrice, String prodDesc, String prodType, Boolean prodFeatured, String prodImg) {
+        this._prodId = _prodId;
+        this.prodName = prodName;
+        this.prodRetailPrice = prodRetailPrice;
+        this.prodSalePrice = prodSalePrice;
+        this.prodDesc = prodDesc;
+        this.prodType = prodType;
+        this.prodFeatured = prodFeatured;
+        this.prodImg = prodImg;
+    }
+
+    public int get_prodId() {return _prodId;}
+    public void set_prodId(int _prodId) {this._prodId = _prodId;}
+
+    public String getProdName() {return prodName;}
+    public void setProdName(String prodName) {this.prodName = prodName;}
+
+    public Double getProdRetailPrice() {return prodRetailPrice;}
+    public void setProdRetailPrice(Double prodRetailPrice) {this.prodRetailPrice = prodRetailPrice;}
+
+    public Double getProdSalePrice() {return prodSalePrice;}
+    public void setProdSalePrice(Double prodSalePrice) {this.prodSalePrice = prodSalePrice;}
+
+    public String getProdDesc() {return prodDesc;}
+    public void setProdDesc(String prodDesc) {this.prodDesc = prodDesc;}
+
+    public String getProdType() {return prodType;}
+    public void setProdType(String prodType) {this.prodType = prodType;}
+
+    public Boolean getProdFeatured() {return prodFeatured;}
+    public void setProdFeatured(Boolean prodFeatured) {this.prodFeatured = prodFeatured;}
+
+    public String getProdImg() {return prodImg;}
+    public void setProdImg(String prodImg) {this.prodImg = prodImg;}
+}
---
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/ProductDao.java
@@ -0,0 +1,35 @@
+package com.rollingpinbakery.rollingpinbakery.Data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+import java.util.List;
+
+/**
+ * Created by rudst on 2/12/2018.
+ */
+@Dao
+public interface ProductDao {
+    @Insert()
+    public void insert(Product product);
+
+    @Update()
+    public void update(Product product);
+
+    @Delete()
+    public void delete(Product product);
+
+    @Delete()
+    public void deleteAll(List<Product> productList);
+
+    @Query("SELECT * FROM product where _id == :id")
+    public Product getProductById(int id);
+
+    @Query("SELECT * FROM product")
+    public List<Product> getAllProducts();
+
+    //@Query("Update product set name = 'test', email = 'test@test.com' where _id == :id")
+    //public void updateProductById(int id);
+}
