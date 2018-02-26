3f32ed976c374cf3c2ec4d716a3c31ca66f92b74
StephanieSchmitz <Stephanie.Schmitz.2@my.sctcc.edu>
2018-02-26 10:46:30 -0600

    Updated Register.java, trying to figure out adding to database.

--- app/src/main/java/com/rollingpinbakery/rollingpinbakery/Register.java
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/Register.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.rollingpinbakery.rollingpinbakery.Data.AppDatabase;
+import com.rollingpinbakery.rollingpinbakery.Data.Customer;
+
 public class Register extends AppCompatActivity {
 
     @Override
@@ -16,25 +19,24 @@ public class Register extends AppCompatActivity {
 
     public void SubmitRegistration(View v) {
         //Gets values
+        EditText fName = findViewById(R.id.fName);
+        EditText lName = findViewById(R.id.lName);
         EditText userName = findViewById(R.id.userName);
         EditText password = findViewById(R.id.password);
         EditText email = findViewById(R.id.email);
-        EditText address = findViewById(R.id.address);
-        EditText city = findViewById(R.id.city);
-        EditText state = findViewById(R.id.state);
-        EditText zip = findViewById(R.id.zipCode);
 
+        String fNameText = fName.getText().toString();
+        String lNameText = lName.getText().toString();
         String userNameText = userName.getText().toString();
         String passwordText = password.getText().toString();
         String emailText = email.getText().toString();
-        String addressText = address.getText().toString();
-        String cityText = city.getText().toString();
-        String stateText = state.getText().toString();
-        String zipText = zip.getText().toString();
 
 
-        //Shows that values have been retrieved
-        Toast.makeText(this, "Welcome " + userNameText, Toast.LENGTH_LONG).show();
+
+        AppDatabase.getAppDatabase(this).customerDao().insert(
+                new Customer(fNameText, lNameText, userNameText, passwordText, emailText, "Customer"));
+        finish();
+
     }
 }
 
