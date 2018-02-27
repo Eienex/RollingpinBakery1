d1c7638eb79cac16d439d7ec95b62309297e62f0
rudsteven <rudsteven@gmail.com>
2018-02-27 11:32:52 -0600

    Added Icons to the left nav bar and created drawers for the Guest, Logged in, and Admin Users/Customers

--- app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/CustomerDao.java
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/CustomerDao.java
@@ -1,6 +1,7 @@
 package com.rollingpinbakery.rollingpinbakery.Data;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
