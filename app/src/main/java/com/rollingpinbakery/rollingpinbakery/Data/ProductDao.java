0d71ca9b2d142e62a1f5d0b66a04c705dad27087
Equnox <Equnox@gmail.com>
2018-02-28 11:41:17 -0600

    Store update Item pages

--- app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/ProductDao.java
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/ProductDao.java
@@ -27,6 +27,9 @@ public interface ProductDao {
     @Query("SELECT * FROM product where _prodId == :id")
     public Product getProductById(int id);
 
+    @Query("SELECT * FROM product where prodType == :type")
+    public Product getProductByType(String type);
+
     @Query("SELECT * FROM product")
     public List<Product> getAllProducts();
 
