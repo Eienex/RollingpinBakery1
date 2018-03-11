fc18802069e509aa7c54724b38efdd911b689e7a
StephanieSchmitz <Stephanie.Schmitz.2@my.sctcc.edu>
2018-03-11 16:36:44 -0500

    Reports page, plus each page for different reports

--- app/src/main/java/com/rollingpinbakery/rollingpinbakery/CustReports.java
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/CustReports.java
@@ -2,23 +2,27 @@ package com.rollingpinbakery.rollingpinbakery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.rollingpinbakery.rollingpinbakery.Data.AppDatabase;
 import com.rollingpinbakery.rollingpinbakery.Data.Customer;
+import com.rollingpinbakery.rollingpinbakery.R;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CustReports extends AppCompatActivity {
 
     ListView listView;
     ArrayList<Customer> customers;
-    private static Cust_ReportAdapter adapter;
+    private static CustomerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_reports_customer);
+        setContentView(R.layout.activity_cust_reports);
 
     }
 
@@ -27,7 +31,7 @@ public class CustReports extends AppCompatActivity {
         super.onResume();
         listView = findViewById(R.id.list_item);
         customers = (ArrayList<Customer>) AppDatabase.getAppDatabase(this).customerDao().getCustReports();
-        adapter = new Cust_ReportAdapter(this, customers);
+        adapter = new CustomerAdapter(this, customers);
         listView.setAdapter(adapter);
     }
 
