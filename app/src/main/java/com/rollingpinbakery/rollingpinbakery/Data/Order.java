3882d4648cee26edf549cc7f133130b512c8426b
rudsteven <rudsteven@gmail.com>
2018-03-12 09:53:24 -0500

    Fixed Order class and orderDao

--- app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/Order.java
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/Order.java
@@ -18,8 +18,7 @@ public class Order {
 
     @ColumnInfo(name = "custId")
     private int custId;
-
-
+    
     public Order(int _orderId, int paymentId, int custId) {
         this._orderId = _orderId;
         this.paymentId = paymentId;
