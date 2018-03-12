4138f8d57c898653b84421c2e9359c1ed8a3d0c3
StephanieSchmitz <Stephanie.Schmitz.2@my.sctcc.edu>
2018-03-11 23:06:13 -0500

    Created Order Table, OrderDao, and got the OrderReport page ready for when there is orders in database

---
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/Data/OrderDao.java
@@ -0,0 +1,34 @@
+package com.rollingpinbakery.rollingpinbakery.Data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface OrderDao {
+    @Insert()
+    public void insert(Order order);
+
+    @Update()
+    public void update(Order order);
+
+    @Delete()
+    public void delete(Order order);
+
+    @Delete()
+    public void deleteAll(List<Order> orderList);
+
+    @Query("SELECT * FROM `order` where _orderId == :id")
+    public Order getOrderById(int id);
+
+    @Query(value = "SELECT * FROM 'order'")
+    public List<Order> getAllOrders();
+
+    @Query("SELECT _orderId, custId FROM 'order'")
+    public List<Order> getOrderReports();
+
+}
--- app/src/main/java/com/rollingpinbakery/rollingpinbakery/Order_ReportAdapter.java
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/Order_ReportAdapter.java
@@ -6,21 +6,43 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import com.rollingpinbakery.rollingpinbakery.Data.Customer;
-import com.rollingpinbakery.rollingpinbakery.Data.Product;
+import com.rollingpinbakery.rollingpinbakery.Data.Order;
 
 import java.util.ArrayList;
 
-/**
- * Created by jamej on 3/11/2018.
- */
 
-public class Order_ReportAdapter {
+public class Order_ReportAdapter  extends ArrayAdapter<Order> {
 
-    private ArrayList<Product> dataSet;
+    private ArrayList<Order> dataSet;
     Context context;
 
+    public Order_ReportAdapter(Context context, ArrayList<Order> orders){
+        super(context, R.layout.o_reports_listview, orders);
+        this.dataSet = orders;
+        this.context = context;
+    }
 
+
+    @NonNull
+    @Override
+    public View getView(int postition, @Nullable View convertView, @NonNull final ViewGroup parent){
+        final Order order = getItem(postition);
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        convertView = inflater.inflate(R.layout.o_reports_listview, parent, false);
+
+        final TextView OrderID = convertView.findViewById(R.id.OrderId);
+        final TextView PaymentID = convertView.findViewById(R.id.PaymentId);
+        final TextView CustID = convertView.findViewById(R.id.CustId);
+
+
+
+        OrderID.setText("Order ID: " + order.get_orderId());
+        PaymentID.setText("Payment ID: " + order.getPaymentId());
+        CustID.setText("Customer ID: " + order.getCustId());
+
+        return convertView;
+    }
 }
