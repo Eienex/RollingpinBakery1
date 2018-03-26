19e5f500e9d7bfc61ec9e9a8eacfe42036981b14
rudsteven <rudsteven@gmail.com>
2018-03-26 10:26:10 -0500

    fixed Bugs: Categories kept crashing
    Added a piece of code that adds a sample cookie into the cookie category for testing purposes

--- app/src/main/java/com/rollingpinbakery/rollingpinbakery/CookiesCategory.java
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/CookiesCategory.java
@@ -41,16 +41,9 @@ public class CookiesCategory extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        AppDatabase.getAppDatabase(this).productDao().insert(
+                new Product("Simple Cookie",0.99,null,"A sample cookie","Cookie",false,null));
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         //get shared Preferences
         sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
--- app/src/main/java/com/rollingpinbakery/rollingpinbakery/DonutCategory.java
+++ app/src/main/java/com/rollingpinbakery/rollingpinbakery/DonutCategory.java
@@ -41,14 +41,6 @@ public class DonutCategory extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         //get shared Preferences
         sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
